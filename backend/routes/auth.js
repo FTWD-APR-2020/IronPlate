f9ae8abd15cd213834b2460c31b35f7c222c13e6
gabrielacarr <briecardo@icloud.com>
2020-05-21 10:06:44 -0400

    remove

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -33,14 +33,3 @@ router.get('/logout', (req, res, next) => {
   res.status(200).json({ msg: 'Logged out' });
 });
 
-router.get('/profile', isAuth, (req, res, next) => {
-  User.findById(req.user._id)
-    .then((user) => res.status(200).json({ user }))
-    .catch((err) => res.status(500).json({ err }));
-});
-
-function isAuth(req, res, next) {
-  req.isAuthenticated() ? next() : res.status(401).json({ msg: 'Log in first' });
-}
-
-module.exports = router;
